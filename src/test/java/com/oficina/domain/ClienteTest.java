0ca28cf2b0aae85d579abd0b5c564fd31489a5f6
ycarozn7 <ycarofelipe551@gmail.com>
2026-06-07 04:04:14 -0300

    feat: implement StatusOS enum

--- src/test/java/com/oficina/domain/ClienteTest.java
+++ src/test/java/com/oficina/domain/ClienteTest.java
@@ -1,5 +1,29 @@
 package com.oficina.domain;
 
-public class ClienteTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class ClienteTest {
+
+    @Test
+    void clienteValidoDeveSerCriado() {
+        Telefone tel = new Telefone("11999999999");
+        Cliente c = new Cliente("Giorgian Arrasca", tel);
+        assertEquals("Giorgian Arrasca", c.getNome());
+    }
+
+    @Test
+    void clienteSemNomeDeveLancarExcecao() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Telefone tel = new Telefone("11999999999");
+            new Cliente(null, tel);
+        });
+    }
+
+    @Test
+    void clienteSemTelefoneDeveLancarExcecao() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Cliente("Giorgian Arrasca", null);
+        });
+    }
 }

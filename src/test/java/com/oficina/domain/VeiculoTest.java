97870f3f7bd85f29798665d0fe481c5f7151fa4b
ycarozn7 <ycarofelipe551@gmail.com>
2026-06-07 01:19:48 -0300

    test: add Telefone tests (red)

--- src/test/java/com/oficina/domain/VeiculoTest.java
+++ src/test/java/com/oficina/domain/VeiculoTest.java
@@ -8,15 +8,15 @@ class VeiculoTest {
     @Test
     void veiculoValidoDeveSerCriado() {
         Placa placa = new Placa("ABC1D23");
-        Veiculo v = new Veiculo(placa, "Gol", 2020);
-        assertEquals("Gol", v.getModelo());
-        assertEquals(2015, v.getAno());
+        Veiculo v = new Veiculo(placa, "Uno", 2012);
+        assertEquals("Uno", v.getModelo());
+        assertEquals(2012, v.getAno());
     }
 
     @Test
     void veiculoSemPlacaDeveLancarExcecao() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new Veiculo(null, "uno", 2015);
+            new Veiculo(null, "Uno", 2012);
         });
     }
 
@@ -24,7 +24,7 @@ class VeiculoTest {
     void veiculoSemModeloDeveLancarExcecao() {
         assertThrows(IllegalArgumentException.class, () -> {
             Placa placa = new Placa("ABC1D23");
-            new Veiculo(placa, null, 2015);
+            new Veiculo(placa, null, 2012);
         });
     }
 }
